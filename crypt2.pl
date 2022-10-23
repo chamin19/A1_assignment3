b1df027f777d984985b54219d543bc1706e18711
camillia <hanaan.amin@yahoo.com>
2022-10-23 19:44:21 -0400

    some of part 2

--- crypt2.pl
+++ crypt2.pl
@@ -16,7 +16,6 @@
 dig(0). dig(1). dig(2). dig(3).  dig(4).
 dig(5). dig(6). dig(7). dig(8).  dig(9).
 
-
 %%%%% HELPER PREDICATES
 % Put any helper predicates here (leave empty if you do not have any
 all_diff([]).
@@ -29,7 +28,38 @@ memberList(N,[M|L]) :- memberList(N,L).
 %%%%% RULE: solve( ListOfVars )
 % Add the rules defining your solve program
 % MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
+solve([A,C,H,I,O,R,S,T,W]) :-
+    % generate
+    dig(O), dig(W),  W > 0, dig(S), S > 0,
 
+
+    dig(A), dig(C), dig(H), dig(I), dig(O), dig(R), 
+    dig(T),  dig(W2), dig(A2), dig(R2), 
+    dig(T2), dig(S2), dig(R3), dig(A3), 
+
+    C > 0, T > 0,
+    S=S2, T=T2, R=R2, R=R3, A=A2, A=A3, W=W2,
+
+    % test
+    S is (W*O) mod 10, CA1 is ((W*O) // 10), 
+    T is (A*O + CA1) mod 10, CA10 is ((A*O + CA1) // 10), 
+    R is (R*O + CA10) mod 10, CA100 is ((R*O + CA10) // 10), 
+    A is (T*O + CA100) mod 10, CA1000 is ((T*O + CA100) // 10),
+    W is (S*O + CA1000) mod 10, CA10000 is ((S*O + CA1000) // 10),
+    
+    W2 is (W*T) mod 10, CB1 is (W*T) // 10, 
+    A2 is (A*T + CB1) mod 10, CB10 is (A*T + CB1) // 10, 
+    R2 is (R*T + CB10) mod 10, CB100 is (R*T + CB10) // 10, 
+    T2 is (T*T + CB100) mod 10, CB1000 is (T*T + CB100) // 10,
+    S2 is (S*T + CB1000) mod 10,
+
+    R3 is (W+T) mod 10, CC1 is (W+T) // 10, 
+    I is (R+A + CC1) mod 10, CC10 is (R+A + CC1) // 10, 
+    A3 is (A+R + CC10) mod 10, CC100 is (A+R + CC10) // 10, 
+    H is (W+T + CC100) mod 10, CC1000 is (W+T + CC100) // 10,
+    C is (CA1000 + S + CC1000) mod 10,
+
+    all_diff([A,C,H,I,O,R,S,T,W]).
 
 
 %%%%% RULE: print_solution ( ListOfVars )
