76582bf6157eed21739531e853e84966129134ef
camillia <hanaan.amin@yahoo.com>
2022-11-06 20:31:03 -0500

    final version

--- kenken.pl
+++ kenken.pl
@@ -1,16 +1,3 @@
-% Enter the names of your group members below.
-% If you only have 2 group members, leave the last space blank
-%
-%%%%%
-%%%%% NAME: Camillia Amin 501071556 Section 1
-%%%%% NAME: Rudaba Syed Section 1
-%%%%% NAME: Mayra Mussarrat Section 1
-%
-% Add the required atomic propositions and rules in the corresponding sections. 
-% If you put the propositions and rules in the wrong sections, you may lose marks.
-%
-% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
-
 %%%%% ATOMIC
 % Put any atomic statements needed here
 dig(1). dig(2). dig(3). dig(4).
@@ -85,4 +72,3 @@ write(Row3), nl,
 write(Row4).
 
 %%%%% END
-% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
--- timetable.pl
+++ timetable.pl
@@ -1,19 +1,3 @@
-% Enter the names of your group members below.
-% If you only have 2 group members, leave the last space blank
-%
-%%%%%
-%%%%% NAME: Camillia Amin Section 1
-%%%%% NAME: Rudaba Syed Section 1
-%%%%% NAME: Mayra Mussarrat Section 1
-%
-% Add the required atomic propositions and rules in the corresponding sections. 
-% If you put the propositions and rules in the wrong sections, you may lose marks.
-%
-% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
-
-%%%%% ATOMIC
-% Put any atomic statements needed here
-
 dance([[fri,1], [fri,2], [fri,3]]).
 art([[mon,10], [wed,10], [fri,10]]). art([[mon,11], [wed,11], [fri,11]]).
 math([[mon,11], [wed,3], [fri,3]]). math([[mon,2], [wed,2], [fri,11]]).
@@ -73,4 +57,3 @@ write(' Dance            | '), write(Dance1), write('   | '), write(Dance2), wri
 write(' Lunch            | '), write(Lunch1), write('   | '), write(Lunch2), write('   | '), write(Lunch3), nl.
 
 %%%%% END
-% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
